8aa696918bf88be6104b5ea0da6551428fffe433
Neehar Kondapaneni <nkondapa@caltech.edu>
2022-07-19 14:24:35 -0700

    README rough

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import torch
 
 
-SUBROOT = f'/home/nkondapa/'
-ROOT = f'/home/nkondapa/PycharmProjects/VisualKnowledgeTracing/'
-# SYNTHETIC_DATASET_FOLDER = f'{ROOT}/datasets/'
+SUBROOT = f''
+ROOT = f''
 TRAINED_MODELS_FOLDER = f'{ROOT}/trained_models/'
 VKT_DATASET_FOLDER = f'{SUBROOT}/Datasets/VKT_Datasets/'
 
